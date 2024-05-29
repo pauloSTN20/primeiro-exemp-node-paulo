9a6564fc2981068e361fb2e1593ae93a59bbb8f0
pauloSTN20 <pjo235408@gmail.com>
2024-05-29 11:32:16 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const host = '127.0.0.1';  
+const port = 3001;
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; 
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello word com Node.JS!');
+})
+
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}`);
+})
